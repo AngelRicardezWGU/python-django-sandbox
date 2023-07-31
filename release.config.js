807ece12eb47a58afbbd83c8e37b4779306e446c
AngelRicardezWGU <angel.ricardez@wgu.edu>
2023-07-30 21:10:23 -0600

    first commit

---
+++ release.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          { type: 'refactor', release: 'patch' },
+          { type: 'revert', release: 'patch' },
+          { type: 'style', release: 'patch' },
+          { type: 'release', release: 'major' },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'src/assets/docs/CHANGELOG.md',
+      },
+    ],
+    ['@semantic-release/npm', { npmPublish: false }],
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          'package.json',
+          'package-lock.json',
+          'src/assets/docs/CHANGELOG.md',
+        ],
+        message:
+          'chore(release): Releasing Version: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ]
+};
